5ab17e5bfa627b1db7fcdcced5e414ba14b63cfb
li-zhiyu <1035614736@qq.com>
2020-11-06 17:59:35 +0800

    首页tabcontrol

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tab-control">
+        <div v-for="item in titles" class="tab-control-item">
+            <span>{{item}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabControl",
+        props: {
+            titles: {
+                type: Array,
+                default() {
+                    return []
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .tab-control {
+        height: 44px;
+        line-height: 44px;
+        display: flex;
+        text-align: center;
+        font-size: 15px;
+    }
+    .tab-control-item {
+        flex: 1;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
+    <div id="home">
         <nav-bar class="home-nav">
             <div slot="center">精选</div>
         </nav-bar>
         <home-swiper :banners="banners"/>
         <recommend-view :recommends="recommends"/>
+        <feature-view/>
+        <tab-control :titles="['流行','新款','精选']"/>
+        <ul>
+            <li>1111</li>
+            <li>1111</li>
+            <li>1111</li>
+            <li>1111</li>
+            <li>1111</li>
+            <li>1111</li>
+            <li>1111</li>
+        </ul>
     </div>
 </template>
 
@@ -13,10 +24,12 @@
     import HomeSwiper from "./childComps/HomeSwiper";
     import RecommendView from "./childComps/RecommendView";
     import {getHomeMultiData} from "network/home";
+    import FeatureView from "./childComps/FeatureView";
+    import TabControl from "../../components/content/tabControl/TabControl";
 
     export default {
         name: "home",
-        components: {RecommendView, HomeSwiper, NavBar},
+        components: {TabControl, FeatureView, RecommendView, HomeSwiper, NavBar},
         data() {
             return {
                 banners: [],
@@ -34,7 +47,15 @@
 </script>
 
 <style scoped>
-.home-nav {
-    background-color: #ff8198;
-}
+    .home-nav {
+        background-color: #ff8198;
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 9;
+    }
+    #home {
+        margin-top: 44px;
+    }
 </style>
